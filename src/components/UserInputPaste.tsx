4a34d7acb8cac8dd39390572ed0f2fe7695e2abd
JWorthington97 <jb.worthington@live.co.uk>
2021-08-05 17:38:59 +0100

    Colour scheme

--- src/components/UserInputPaste.tsx
+++ src/components/UserInputPaste.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Textarea, Flex, Button, Input} from '@chakra-ui/react'
+import { Box, Text, Textarea, Flex, Button, Input, Heading} from '@chakra-ui/react'
 import { useState } from 'react'
 import { UserInputPasteProps } from '../utils/Types'
 
@@ -10,15 +10,14 @@ export default function UserInputPaste({postNewPaste}: UserInputPasteProps): JSX
     const [submitResponse, setSubmitResponse] = useState("")
 
     return (
-        <Box w="66%" align="left" pl="4">
-            <Text textAlign="left">Title (optional)</Text>
-            <Input  w="400px" size="sm" onChange={(event) => setTitle(event.target.value)}/>
-            <Text textAlign="left">Paste</Text>
-            <Box h="600px">
-                <Textarea h="580px" w="80%" onChange={(event) => setPaste(event.target.value)}/>
+        <Box w="66%" align="left" >
+            <Heading textAlign="left" mb="4" fontWeight="bold" size="lg" color="#ec643b">New Paste</Heading>
+            <Input  w="400px" size="sm" placeholder="Title (optional)" boxShadow="md" mb="4" backgroundColor="white" onChange={(event) => setTitle(event.target.value)}/>
+            <Box boxShadow="xl" h="600px">
+                <Textarea h="100%" w="100%" resize="none" placeholder="Enter paste here" size="sm" backgroundColor="white" onChange={(event) => setPaste(event.target.value)}/>
             </Box>
-            <Flex>
-                <Button colorScheme="blue" onClick={() => postNewPaste({title, paste, setSubmitResponse})}>Submit</Button>
+            <Flex pt="10">
+                <Button backgroundColor="#f8cb57" onClick={() => postNewPaste({title, paste, setSubmitResponse})}>Submit</Button>
                 {submitResponse !== "" && <Text pl="4" pt="2">{submitResponse}</Text>}
             </Flex>
         </Box>
